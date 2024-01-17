c1c8dafca54ac58e8f6b7cd24e4e58a80bd1723d
Hlaiem <141079608+Hlaiem@users.noreply.github.com>
2024-01-17 23:27:32 +0100

    update nav

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -15,9 +15,6 @@ const Nav = () => {
   const [loggedIn, setLoggedIn] = useState(
     localStorage.getItem("loggedIn") === "true"
   );
-  const [showP, setShowP] = useState(false);
-
-  
   
   const openModal = () => {
     setIsModalOpen(true);
@@ -26,7 +23,6 @@ const Nav = () => {
   const closeModal = () => {
     setIsModalOpen(false);
   };
-  const handleShowP = () => setShowP(true);
 
   const handleSignIn = (success) => {
     setLoggedIn(success);
@@ -59,15 +55,6 @@ const Nav = () => {
 
   const handleShow = () => setShow(true);
   const handleClose = () => setShow(false);
-
-  useEffect(() => {
-    if(showP){
-      handleShow();
-    }
-    else{
-      handleClose();
-    }
-  }, [showP])
 
   return (
     <header
@@ -124,7 +111,7 @@ const Nav = () => {
         onClose={closeModal}
         onSignIn={handleSignIn}
       />
-      <div onClick={handleShowP}>
+      <div onClick={handleShow}>
         <div style={{position:'relative' }}>
         <svg xmlns="http://www.w3.org/2000/svg" width="31"  height="30" fill="white" className="bi bi-cart-fill" viewBox="0 0 16 16">
           <path d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .491.592l-1.5 8A.5.5 0 0 1 13 12H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"/>
@@ -136,14 +123,14 @@ const Nav = () => {
       <div className="hidden lg:flex space-x-4 items-center">
       <div>
 
-<Offcanvas place show={show} placement="end" onHide={handleClose} >
+<Offcanvas show={show} placement="end" onHide={handleClose} >
   <Container style={{ paddingLeft: '40px', display: 'flex', paddingTop: '40px', marginBottom: '0px' }}>
     <h5>SHOPPING CART</h5>
     <div style={{ display: 'flex' }}>
       <h5 style={{ paddingLeft: '90px', paddingRight: '8px', color: 'gray', cursor: 'pointer' }} onClick={handleClose}>close</h5>
       <div style={{ color: 'gray', paddingTop: '0px' }}>
         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-arrow-right" viewBox="0 0 16 16">
-          <path fill-rule="evenodd" d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8z" />
+          <path fillRule="evenodd" d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8z" />
         </svg>
       </div>
     </div>
--- src/components/cart.jsx
+++ src/components/cart.jsx
@@ -47,7 +47,7 @@ export default function Cart() {
                       <img src={cartItem.cover} alt={cartItem.cover} style={{border:'solid gray 1px'}}/>
                       <div style={{width:'100%'}}>
                       <div onClick={() => handleRemoveFromCart(cartItem)} style={{paddingLeft:'200px',  marginTop:'-15px', cursor:'pointer'}}>
-                          <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-trash-fill" viewBox="0 0 16 16">
+                          <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" className="bi bi-trash-fill" viewBox="0 0 16 16">
                           <path d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1H2.5zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5zM8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5zm3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0z"/>
                           </svg>
                         </div>
